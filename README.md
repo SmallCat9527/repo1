394a40e378f9195fbf0b992d1c299b4fcf847cb1
chunni98 <guok.zhao.life@outlook.com>
2022-09-12 18:51:06 +0800

    add hello.py

---
+++ README.md
@@ -0,0 +1,7 @@
+# CrazyCat
+
+闲来无事，写个 “Hello，World！”吧。
+
+## 从 “Hello，world!”开始的编码生活
+
+September 12, 2022 - 新建 hello.py
---
+++ src/hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+#\File        : src/hello.py
+#\Author      : chunni
+#\CreateTime  : 2022-09-12 18:49:29
+#\Version     : 0.1.0
+#\Brief       :
+#\Log         :
+
+def say_sth(x):
+    print(x)
+
+say_sth("Hello,CrazyCat!")
